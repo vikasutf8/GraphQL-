56bc74421607560844f275093c9af1327d91a776
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-07-06 12:06:08 +0530

    mongodb database added

---
+++ models/event.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const evetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Event = mongoose.model("Event", evetSchema);
+
+export default Event;
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import "dotenv/config";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
-import e from "express";
-
+import mongoose from "mongoose";
+import Event from "./models/event.model.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -47,18 +47,42 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        // return events;
+       return Event.find().then(res=>{
+                return res.map(event =>{
+                    // return {...event._doc, _id : event._doc._id.toString()};
+                    return {...event._doc, _id :event.id};
+                })
+            }
+        ).catch(err=>{
+            console.log(err);
+            throw new Error(err);
+        })
       },
-      createEvent: (args) => {
-        const event ={
-            _id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+      createEvent: args => {
+        // const event ={
+        //     _id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+        //     name: args.eventInput.name,
+        //     description: args.eventInput.description,
+        //     price: +args.eventInput.price,
+        //     date: args.eventInput.date,
+        // }
+        const event =new Event({
             name: args.eventInput.name,
             description: args.eventInput.description,
-            price: +args.eventInput.price,
-            date: args.eventInput.date,
-        }
-        events.push(event);
-        return event;
+            price: +args.eventInput.price,            
+            date:new Date(args.eventInput.date),
+        })
+        // events.push(event);
+        return event.save().then(res =>{
+            console.log(res);
+            return {...res._doc, _id : res._doc._id.toString()};
+            // return {...res._doc, _id : res.id};
+        }).catch(err =>{
+            console.log(err);
+            throw new Error(err);
+        });
+       
       },
     },
     graphiql: true,
@@ -69,6 +93,15 @@ app.use(
 //     res.send('Hello World!');
 // });
 
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to MongoDB");
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+}).catch((err) => {
+console.log("Error connecting to MongoDB");
+
+  console.log(err);
+});
+
