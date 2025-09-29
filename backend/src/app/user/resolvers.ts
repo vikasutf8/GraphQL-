ab7c6e78246b068611c541aff0bf051eb0d7d99c
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-09-29 16:49:15 +0530

    Fix: GetUser backend great working

--- backend/src/app/user/resolvers.ts
+++ backend/src/app/user/resolvers.ts
@@ -1,81 +1,81 @@
-import axios from 'axios';
-import prisma from '../../clients/db';
-import JwtService from '../../services/jwt';
-import { GraphqlContext } from '../../interface';
-
-
+import axios from "axios";
+import prisma from "../../clients/db";
+import JwtService from "../../services/jwt";
+import { GraphqlContext } from "../../interface";
 
 export interface GoogleTokenResult {
-    iss?: string;
-    azp?: string;
-    aud?: string;
-    sub?: string;
-    email: string;
-    email_verified: string; 
-    nbf?: string;
-    name?: string;
-    picture?: string;
-    given_name: string; 
-    family_name?: string;
-    iat?: string;
-    exp?: string;
-    jti?: string;
-    alg?: string;
-    kid?: string;
-    typ?: string;
-  }
-
-const queries={
-    verifyGoogleToken: async(parent:any,{token}:{token:string}) => {
-
-        const googleToken = token;
-        const googleOAuthUrl= new URL('https://oauth2.googleapis.com/tokeninfo');
-        googleOAuthUrl.searchParams.set('id_token', googleToken);
-        const response = await axios.get<GoogleTokenResult>(googleOAuthUrl.toString(),{
-            responseType: 'json'
-        });
-       
-        const checkForUser =await prisma.user.findUnique({
-            where:{
-                email:response.data.email
-            }
-        });
-
-        if(!checkForUser){
-            await prisma.user.create({
-                data:{
-                    firstName:response.data.given_name,
-                    lastName:response.data.family_name,
-                    email:response.data.email,
-                    profileImageUrl:response.data.picture
-                } 
-            });
-        }
-        const userInDb =await prisma.user.findUnique({
-            where:{
-                email:response.data.email
-            }
-        }); 
-
-        const userToken =await JwtService.generateTokenForUser(userInDb!); 
-         return userToken;
-    },
-
-    // getCurrentUser: async(parent:any,args:any,context:GraphqlContext) => {
-    //     // console.log(context)
-    //     const id = context.user?.id;
-    //     if(!id)return null;
-    //     // return context.user;
-    //     const user = await prisma.user.findUnique({
-    //         where:{
-    //             id
-    //         }
-    //     });
-    //     return user;
-    // }
-} 
-
-export const resolvers={
-    queries
+  iss?: string;
+  azp?: string;
+  aud?: string;
+  sub?: string;
+  email: string;
+  email_verified: string;
+  nbf?: string;
+  name?: string;
+  picture?: string;
+  given_name: string;
+  family_name?: string;
+  iat?: string;
+  exp?: string;
+  jti?: string;
+  alg?: string;
+  kid?: string;
+  typ?: string;
 }
-  
+
+const queries = {
+  verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
+    const googleToken = token;
+    const googleOAuthUrl = new URL("https://oauth2.googleapis.com/tokeninfo");
+    googleOAuthUrl.searchParams.set("id_token", googleToken);
+    const response = await axios.get<GoogleTokenResult>(
+      googleOAuthUrl.toString(),
+      {
+        responseType: "json",
+      }
+    );
+
+    const checkForUser = await prisma.user.findUnique({
+      where: {
+        email: response.data.email,
+      },
+    });
+
+    if (!checkForUser) {
+      await prisma.user.create({
+        data: {
+          firstName: response.data.given_name,
+          lastName: response.data.family_name,
+          email: response.data.email,
+          profileImageUrl: response.data.picture,
+        },
+      });
+    }
+    const userInDb = await prisma.user.findUnique({
+      where: {
+        email: response.data.email,
+      },
+    });
+
+    const userToken = await JwtService.generateTokenForUser(userInDb!);
+    return userToken;
+  },
+
+    getCurrentUser: async (parent: any, args: any, context: GraphqlContext) => {
+        
+        const id = context.user?.id;
+        console.log(id)
+        if (!id) return null;
+        // return context.user;
+        const user = await prisma.user.findUnique({
+        where: {
+            id,
+        },
+        });
+        return user;
+    },
+};
+
+export const resolvers = {
+  queries,
+};
